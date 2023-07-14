68d75972cbeba008adf003f68f9094cbae3b4314
CJiu01 <jiwoo020801@gmail.com>
2023-07-14 16:03:28 +0900

    [FEAT] ImageLiterals 추가 (#3)

--- Flag-iOS/Flag-iOS/Global/Literal/ImageLiterals.swift
+++ Flag-iOS/Flag-iOS/Global/Literal/ImageLiterals.swift
@@ -5,4 +5,26 @@
 //  Created by 최지우 on 2023/07/14.
 //
 
-import Foundation
+import UIKit
+
+enum ImageLiterals {
+    
+    // MARK:
+    
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        return image
+    }
+    
+    static func load(systemName: String) -> UIImage {
+        guard let image = UIImage(systemName: systemName, compatibleWith: nil) else {
+            return UIImage()
+        }
+        return image
+    }
+}
