f9af2a5715647106097568d3f1107f3dc5db68cd
CJiu01 <jiwoo020801@gmail.com>
2023-07-16 18:19:56 +0900

    [Feat] UIFont+ 추가 (#3)

---
+++ Flag-iOS/Flag-iOS/Global/Extension/UIFont+.swift
@@ -0,0 +1,54 @@
+//
+//  FontLiterals.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/07/14.
+//
+
+import UIKit
+
+extension UIFont {
+    class var head1: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 20)!
+    }
+    
+    class var head2: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 16)!
+    }
+    
+    class var title1: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 18)!
+    }
+    
+    class var title2: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 14)!
+    }
+    
+    class var title3: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 12)!
+    }
+    
+    class var subTitle1: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 18)!
+    }
+    
+    class var subTitle2: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 16)!
+    }
+    
+    class var subTitle3: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 14)!
+    }
+    
+    class var body1: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 16)!
+    }
+    
+    class var body2: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 12)!
+    }
+    
+    class var body3: UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: 10)!
+    }
+}
