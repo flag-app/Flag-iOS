6f8219b6080bb0c7d221628e4a9c38a779135ed8
CJiu01 <jiwoo020801@gmail.com>
2023-07-16 19:08:26 +0900

    [Feat] UIView extension 추가 (#5)

---
+++ Flag-iOS/Flag-iOS/Global/Extension/UIView+.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/07/16.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
