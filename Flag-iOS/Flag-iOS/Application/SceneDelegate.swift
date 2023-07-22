19c42d5ea9eea99e836909a095a2192a42a525b3
JuHyeonAh <jha08130930@gmail.com>
2023-07-22 16:45:48 +0900

    [SETTING] 추가 프로젝트 세팅(#8)

--- Flag-iOS/Flag-iOS/Application/SceneDelegate.swift
+++ Flag-iOS/Flag-iOS/Application/SceneDelegate.swift
@@ -10,8 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -22,6 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         navigationController.isNavigationBarHidden = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
+        // 다크 모드 해제
+        window?.overrideUserInterfaceStyle = .light
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
