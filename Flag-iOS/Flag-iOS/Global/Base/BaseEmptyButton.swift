d93bcd7c49fd05a418fa8a23b51a815b40c8afea
CJiu01 <jiwoo020801@gmail.com>
2023-08-02 23:00:22 +0900

    [FEAT] BaseFillButton/BaseEmptyButton 구현완료 (#11)

--- Flag-iOS/Flag-iOS/Global/Base/BaseEmptyButton.swift
+++ Flag-iOS/Flag-iOS/Global/Base/BaseEmptyButton.swift
@@ -5,4 +5,23 @@
 //  Created by 최지우 on 2023/08/02.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class BaseEmptyButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupEmptyButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupEmptyButton() {
+        self.addTitleAttribute(title: "", titleColor: .purpleDefault, fontName: .title1)
+        self.setRoundBorder(borderColor: .purpleDefault, borderWidth: 1.0, cornerRadius: 16)
+    }
+}
--- Flag-iOS/Flag-iOS/Global/Base/BaseFillButton.swift
+++ Flag-iOS/Flag-iOS/Global/Base/BaseFillButton.swift
@@ -5,4 +5,24 @@
 //  Created by 최지우 on 2023/08/02.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+class BaseFillButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupFillButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupFillButton() {
+        self.addTitleAttribute(title: "", titleColor: .white, fontName: .title1)
+        self.backgroundColor = .purpleDefault
+        self.layer.cornerRadius = 16
+    }
+}
