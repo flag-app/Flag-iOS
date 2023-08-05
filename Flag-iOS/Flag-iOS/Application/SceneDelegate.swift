cdf7be0a862f653ae76d0e7e46a1284b6025e661
CJiu01 <jiwoo020801@gmail.com>
2023-08-05 20:27:47 +0900

    [SETTING] navi hidden처리 (#17)

--- Flag-iOS/Flag-iOS/Application/SceneDelegate.swift
+++ Flag-iOS/Flag-iOS/Application/SceneDelegate.swift
@@ -18,11 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         let navigationController = UINavigationController(rootViewController: OnboardingViewController())
-        navigationController.isNavigationBarHidden = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
         // 다크 모드 해제
         window?.overrideUserInterfaceStyle = .light
+        // navi hidden처리
+        navigationController.isNavigationBarHidden = true
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
