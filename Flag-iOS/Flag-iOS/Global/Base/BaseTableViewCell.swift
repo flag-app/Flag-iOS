1ef8edd5c02b9225475c5d8cc7c5fe50381ea934
CJiu01 <jiwoo020801@gmail.com>
2023-08-12 00:43:43 +0900

    [FEAT] BaseTableViewCell 구현 (#33)

---
+++ Flag-iOS/Flag-iOS/Global/Base/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/08/12.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {}
+    
+    func setLayout() {}
+    
+}
---
+++ Flag-iOS/Flag-iOS/Scenes/Flag/Views/FlagMain/FlagCell.swift
@@ -0,0 +1,8 @@
+//
+//  FlagCell.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/08/12.
+//
+
+import Foundation
