0128dccbc4588e7d91c08f2835e29f43eae209eb
CJiu01 <jiwoo020801@gmail.com>
2023-08-15 05:44:02 +0900

    [FEAT] FlagInfoView 구현 (#38)

---
+++ Flag-iOS/Flag-iOS/Scenes/Flag/ViewCotrollers/FlagMain/FlagInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlagInfoViewController.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/08/15.
+//
+
+import UIKit
+
+class FlagInfoViewController: BaseUIViewController {
+    
+    // MARK: - UI Components
+    
+    let flagInfoView = FlagInfoView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray100
+    }
+    
+    override func setUI() {
+        view.addSubview(flagInfoView)
+    }
+    
+    override func setLayout() {
+        flagInfoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
