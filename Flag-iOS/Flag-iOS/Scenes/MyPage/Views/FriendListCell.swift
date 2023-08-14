0f9e5ae83f6c9f768f23bc0ed15251142285f7bd
hyeonju <shj040201@naver.com>
2023-08-15 06:44:53 +0900

    [Feat]친구리스트 테이블뷰 추가/#42

---
+++ Flag-iOS/Flag-iOS/Scenes/MyPage/Views/FriendListCell.swift
@@ -0,0 +1,73 @@
+//
+//  FriendListCell.swift
+//  Flag-iOS
+//
+//  Created by 성현주 on 2023/08/15.
+//
+
+import UIKit
+
+import SnapKit
+
+class FriendListCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
+   
+    // MARK: - UI Components
+   
+    lazy var deleteButton: UIButton = {
+        let button = UIButton()
+        button.addTitleAttribute(title: "삭제", titleColor: .black, fontName: .body2)
+        button.backgroundColor = .gray200
+        button.layer.cornerRadius = 6
+        return button
+    }()
+    
+    private let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.profileImage
+        return imageView
+    }()
+    
+    // MARK: - Custom Method
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        titleLabel.font = .subTitle3
+        subtitleLabel.font = .body2
+        
+        contentView.addSubviews(deleteButton,
+                                titleLabel,
+                                subtitleLabel,
+                                profileImage)
+        
+        
+        deleteButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(290)
+            make.trailing.equalToSuperview().inset(23)
+            make.centerY.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImage.snp.trailing).offset(20)
+            make.top.equalToSuperview().offset(8)
+        }
+        subtitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImage.snp.trailing).offset(20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+        }
+        profileImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(25)
+            make.width.height.equalTo(36)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
---
+++ Flag-iOS/Flag-iOS/Scenes/MyPage/Views/FriendListView.swift
@@ -0,0 +1,70 @@
+//
+//  FriendListView.swift
+//  Flag-iOS
+//
+//  Created by 성현주 on 2023/08/15.
+//
+
+import UIKit
+
+import SnapKit
+
+final class FriendListView: BaseUIView {
+    
+    // MARK: - Properties
+
+    private let leadingWidth = 25
+    
+    // MARK: - UI Components
+    
+    private let friendSearchTextField: BaseUITextField = {
+        let textField = BaseUITextField()
+        textField.placeholder = TextLiterals.flagFriendsHintText
+        return textField
+    }()
+    
+    lazy var searchButton: BaseFillButton = {
+        let button = BaseFillButton()
+        button.setTitle(TextLiterals.searchText, for: .normal)
+        button.layer.cornerRadius = 6
+        button.isEnabled = true
+        return button
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 60
+        tableView.register(FriendListCell.self, forCellReuseIdentifier: "FriendListCell")
+        return tableView
+    }()
+
+    // MARK: - Custom Method
+    
+    override func setUI() {
+        self.addSubviews(friendSearchTextField,
+                         searchButton,
+                         tableView)
+        
+    }
+    
+    override func setLayout() {
+        friendSearchTextField.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(40)
+            $0.leading.equalToSuperview().inset(23)
+            $0.trailing.equalToSuperview().inset(120)
+            $0.height.equalTo(41)
+        }
+        searchButton.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(40)
+            make.leading.equalToSuperview().inset(290)
+            make.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(41)
+        }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(friendSearchTextField.snp.bottom).offset(15)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
