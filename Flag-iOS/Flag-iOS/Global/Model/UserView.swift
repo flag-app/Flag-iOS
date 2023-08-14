be8c48186531ebdc1c1a76ef20969129169474ad
CJiu01 <jiwoo020801@gmail.com>
2023-08-15 06:53:32 +0900

    [FEAT] UserView UI 구현완료 (#38)

--- Flag-iOS/Flag-iOS/Global/Model/UserView.swift
+++ Flag-iOS/Flag-iOS/Global/Model/UserView.swift
@@ -1,47 +1,47 @@
 //
-//  UserView.swift
+//  User.swift
 //  Flag-iOS
 //
 //  Created by 최지우 on 2023/08/15.
 //
-
 import UIKit
 
 class UserView: BaseUIView {
-    
+
     // MARK: - UI Components
     private let userProfileImage: UIImageView = {
         let imageView = UIImageView()
         imageView.image = ImageLiterals.userProfile
         return imageView
     }()
-    
+
     private let userNameLabel: UILabel = {
         let label = UILabel()
         label.font = .body2
         return label
     }()
-    
+
     init(userName: String, frame: CGRect = .zero) {
         super.init(frame: frame)
         self.userNameLabel.text = userName
+        
         setUI()
         setLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func setUI() {
         addSubviews(userProfileImage,
                     userNameLabel)
     }
-    
+
     override func setLayout() {
         userProfileImage.snp.makeConstraints {
             $0.top.leading.equalToSuperview()
-            
+
         }
         userNameLabel.snp.makeConstraints {
             $0.top.equalTo(userProfileImage.snp.bottom).offset(5)
