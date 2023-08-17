d57187c4d5c6cae914b228c9ef71ba2ad7142bcd
CJiu01 <jiwoo020801@gmail.com>
2023-08-18 01:32:32 +0900

    [SETTING] realm 폴더링 (#65)

---
+++ Flag-iOS/Flag-iOS/Realm/RealmDTO/testRealmViewController.swift
@@ -0,0 +1,49 @@
+//
+//  testRealmViewController.swift
+//  Flag-iOS
+//
+//  Created by 최지우 on 2023/08/18.
+//
+
+import UIKit
+
+import RealmSwift
+
+class testRealmViewController: UIViewController {
+    
+    let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+
+        let jay = UserFriendList()
+        jay.userName = "최지우"
+        jay.userId = "jay"
+        
+        let hy = UserFriendList()
+        hy.userName = "성현주"
+        hy.userId = "hy"
+
+        try! realm.write {
+            realm.add(jay)
+            realm.add(hy)
+        }
+//
+        let savedFriendsList = realm.objects(UserFriendList.self)
+        
+        view.backgroundColor = .systemBackground
+       
+        print(savedFriendsList)
+//        try! realm.write {
+//            realm.deleteAll()
+//        }
+        
+    }
+}
+
+class UserFriendList: Object {
+    @objc dynamic var userName: String = ""
+    @objc dynamic var userId: String = ""
+}
