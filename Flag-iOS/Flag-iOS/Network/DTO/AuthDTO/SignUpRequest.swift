7d934330bdcc8386f3bef45bfa3081d467fc1ea5
CJiu01 <jiwoo020801@gmail.com>
2023-08-21 05:13:32 +0900

    [FEAT] SignUpRequest 생성 (#33)

--- Flag-iOS/Flag-iOS/Network/DTO/AuthDTO/SignUpRequest.swift
+++ Flag-iOS/Flag-iOS/Network/DTO/AuthDTO/SignUpRequest.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct SignUpRequest: Codable {
+    var email: String
+    var name: String
+    var password: String
+    var profile: String
+    
+    init(_ email: String, _ name: String, _ password: String) {
+        self.email = email
+        self.name = name
+        self.password = password
+        self.profile = ""
+    }
+    
+}
