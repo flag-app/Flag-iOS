48343d6b0f1e2ce336bfb37a9a78a6952552f5a6
CJiu01 <jiwoo020801@gmail.com>
2023-08-21 07:53:13 +0900

    [FEAT] Login API 테스트 (#55)

--- Flag-iOS/Flag-iOS/Scenes/Onboarding/ViewControllers/SignInViewController.swift
+++ Flag-iOS/Flag-iOS/Scenes/Onboarding/ViewControllers/SignInViewController.swift
@@ -8,14 +8,22 @@ import Foundation
 
 import SnapKit
 import UIKit
+import Moya
 
 class SignInViewController: BaseUIViewController {
+    
+    let authProvider = MoyaProvider<AuthAPI>()
         
     // MARK: - UI Components
     
     private let signInView = SignInView()
     
     // MARK: - Custom Method
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        login()
+    }
     
     override func setupNavigationBar() {
         super.setupNavigationBar()
@@ -44,4 +52,43 @@ class SignInViewController: BaseUIViewController {
         self.navigationController?.pushViewController(tabBarController, animated: true)
     }
     
+  
+
+}
+
+
+extension SignInViewController {
+    
+    func login() {
+        let param = SignInRequest(email: "oo@naver.com", password: "oo")
+        
+        self.authProvider.request(.signIn(body: param)) { response in
+            switch response {
+            case .success(let moyaResponse):
+                do {
+                    /// SUCCESS
+                    print("Response status code:", moyaResponse.statusCode)
+                    
+                    // moyaResponse.data 를 사용하여 응답 데이터를 파싱합니다.
+//                    let decoder = JSONDecoder()
+//                    let signInResponse = try decoder.decode(SignInResponse.self, from: moyaResponse.data)
+//                    let result = try moyaResponse.map(SignInResponse.self)
+                    
+//                    print("Access Token:", moyaResponse.data)
+                    if let accessToken = String(data: moyaResponse.data, encoding: .utf8) {
+                        print("Access Token:", accessToken)
+                    } else {
+                        print("Failed to decode token.")
+                    }
+
+                } catch let parsingError {
+                    print("Error parsing:", parsingError)
+                }
+            case .failure(let err):
+                /// ERROR
+                print("Error:", err.localizedDescription)
+            }
+        }
+        print("❌login 실행")
+    }
 }
