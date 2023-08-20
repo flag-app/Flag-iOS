78fdfd49097fe6c0b10ec44703e8209645626b22
hyeonju <shj040201@naver.com>
2023-08-20 22:25:01 +0900

    [FEAT] 마이페이지 닉네임변경 뷰 추가

---
+++ Flag-iOS/Flag-iOS/Scenes/MyPage/Views/NicknameChangeView.swift
@@ -0,0 +1,93 @@
+//
+//  NicknameChangeView.swift
+//  Flag-iOS
+//
+//  Created by 성현주 on 2023/08/20.
+//
+
+import UIKit
+
+final class NicknameChangeView: BaseUIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    lazy var nextButton: BaseFillButton = {
+        let button = BaseFillButton()
+        button.setTitle(TextLiterals.nextText, for: .normal)
+        return button
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = TextLiterals.newNicknameText
+        label.font = .title1
+        return label
+    }()
+    
+    private let nameTextField: BaseUITextField = {
+        let textField = BaseUITextField()
+        textField.placeholder = TextLiterals.newNicknameTextHint
+        return textField
+    }()
+    
+    lazy var progressView: UIProgressView = {
+        let view = UIProgressView()
+        view.trackTintColor = .gray200
+        view.progressTintColor = .purple300
+        view.progress = 0.0
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addTarget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Custom Method
+    
+    override func setUI() {
+        self.addSubviews(nextButton,
+                         nameTextField,
+                         nameLabel,
+                         progressView)
+    }
+    override func setLayout() {
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(21)
+            $0.horizontalEdges.equalToSuperview().inset(25)
+            $0.height.equalTo(49)
+        }
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(40)
+            $0.leading.equalToSuperview().offset(25)
+        }
+        nameTextField.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(7)
+            $0.horizontalEdges.equalToSuperview().inset(25)
+            $0.height.equalTo(41)
+        }
+    }
+    
+    func addTarget() {
+        nameTextField.addTarget(self, action: #selector(isChangedValue(_:)), for: .editingChanged)
+    }
+    
+    @objc
+        func isChangedValue(_ textField: UITextField) {
+            if let flagName = nameTextField.text,(flagName.count > 2 && flagName.count < 15) {
+                nextButton.isEnabled = true
+            } else {
+                nextButton.isEnabled = false
+            }
+        }
+}
+
