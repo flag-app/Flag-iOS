6d4ce9ae4c7b10daa787f4ccbf7b0e2b66692882
CJiu01 <jiwoo020801@gmail.com>
2023-08-24 03:07:38 +0900

    [FEAT] MyPageAPI 작성 완료 (#105)

--- Flag-iOS/Flag-iOS/Network/API/MyPageAPI.swift
+++ Flag-iOS/Flag-iOS/Network/API/MyPageAPI.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import Moya
+
+enum MyPageAPI {
+    case getNickname
+}
+
+extension MyPageAPI: BaseTargetType {
+    
+    var path: String {
+        switch self {
+        case .getNickname:
+            return "/user/mypage"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getNickname:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getNickname:
+            return .requestPlain
+        }
+    }
+}
