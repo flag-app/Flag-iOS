de4574084ddd7cd781b82d753edb318a178a3c08
CJiu01 <jiwoo020801@gmail.com>
2023-08-29 22:22:29 +0900

    [FIX] SignUpRequestDTO 수정 (#118)

--- Flag-iOS/Flag-iOS/Network/DTO/AuthDTO/SignUpRequest.swift
+++ Flag-iOS/Flag-iOS/Network/DTO/AuthDTO/SignUpRequest.swift
@@ -11,12 +11,5 @@ struct SignUpRequest: Codable {
     var email: String
     var name: String
     var password: String
-    var profile: String
     
-    init(_ email: String, _ name: String, _ password: String) {
-        self.email = email
-        self.name = name
-        self.password = password
-        self.profile = ""
-    }
 }
